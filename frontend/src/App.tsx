e87a1ff68b9342289f7b39f207072f38d465df0b
Adrian Kahali <adrian.kahali@hotmail.com>
2025-11-21 16:38:39 -0500

    initial setup for frontend with agent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Container, Header, MessageList, Composer, useWebchat } from '@botpress/webchat'
+import { useMemo } from 'react'
 import './App.css'
 
+const headerConfig = {
+  botName: 'Assistant',
+  botAvatar: 'https://files.bpcontent.cloud/2025/11/19/21/20251119210301-2SLGBPIY.png',
+  botDescription: 'Ask a question about the documentation.',
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const { client, messages, isTyping, user, clientState, newConversation } = useWebchat({
+    clientId: import.meta.env.VITE_AGENT_CLIENT_ID
+  })
+
+  const config = {
+    botName: 'Assistant',
+    botAvatar: 'https://files.bpcontent.cloud/2025/11/19/21/20251119210301-2SLGBPIY.png',
+    botDescription: 'Ask a question about the documentation. Powered by Botpress.',
+  }
+  const enrichedMessages = useMemo(
+  () =>
+    messages.map((message) => {
+      const { authorId } = message
+      const direction: 'outgoing' | 'incoming' = authorId === user?.userId ? 'outgoing' : 'incoming'
+      return {
+        ...message,
+        direction,
+        sender:
+          direction === 'outgoing'
+            ? { name: 'You', avatar: undefined }
+            : { name: config.botName ?? 'Bot', avatar: config.botAvatar },
+      }
+    }),
+  [config.botAvatar, config.botName, messages, user?.userId]
+  )
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Container
+      connected={clientState !== 'disconnected'}
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+      }}
+    >
+      <Header
+        defaultOpen={false}
+        closeWindow={() => console.log(close)}
+        restartConversation={newConversation}
+        disabled={false}
+        configuration={headerConfig}
+      />
+      <MessageList
+        botName={config.botName}
+        botDescription={config.botDescription}
+        isTyping={isTyping}
+        showMarquee={false}
+        messages={enrichedMessages}
+        sendMessage={client?.sendMessage}
+      />
+      <Composer
+        disableComposer={false}
+        isReadOnly={false}
+        allowFileUpload={true}
+        connected={clientState !== 'disconnected'}
+        sendMessage={client?.sendMessage}
+        uploadFile={client?.uploadFile}
+        composerPlaceholder="Type a message..."
+      />
+    </Container>
     </>
   )
 }
