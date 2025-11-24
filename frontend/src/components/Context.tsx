cf3a65457b5f6ec2562f503b4f7562e438d85c68
Adrian Kahali <adrian.kahali@hotmail.com>
2025-11-24 10:21:15 -0500

    update context style

--- frontend/src/components/Context.tsx
+++ frontend/src/components/Context.tsx
@@ -15,6 +15,10 @@ const Context = ({currentContext, setCurrentContext}: ContextProps) => {
                 <div
                     className="bpComposerFileAttachement"
                     key={index}
+                    style={{
+                        backgroundColor: "rgb(0 144 255/.1)",
+                        border: "none"
+                    }}
                     >
                     <div
                         style={{
@@ -26,8 +30,8 @@ const Context = ({currentContext, setCurrentContext}: ContextProps) => {
                             color: "var(--bpGray-900)"
                         }}
                     >
-                    <div className="bpComposerFileName" style={{maxWidth: 'none'}}>{context.title}</div>
-                    <div className="bpComposerFileExtension">{context.path}</div>
+                    <div className="bpComposerFileName" style={{maxWidth: 'none', color: "rgb(0 144 255/1)", fontWeight: "600",}}>{context.title}</div>
+                    <div className="bpComposerFileExtension" style={{fontWeight: "400"}}>{context.path}</div>
                     </div>
                     <svg onClick={() => removeContext(index)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-circle-x bpComposerFileRemoveIcon" role="button" tabIndex={0} aria-label="Remove File Button"><circle cx="12" cy="12" r="10"></circle><path d="m15 9-6 6"></path><path d="m9 9 6 6"></path></svg>
                 </div>
