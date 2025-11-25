a8b5a9e018d9086bf7bdc01203eb075ab2e5c9a7
Adrian Kahali <adrian.kahali@hotmail.com>
2025-11-25 17:53:10 -0500

    add gh pages config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/docs-bot/', // Replace with your actual repository name
   plugins: [react()],
   resolve: {
     dedupe: ['react', 'react-dom'],
